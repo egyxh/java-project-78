d419ac422a169d60b398ce09b73fa9cf0e79ff03
egor_semichev <thebestists@gmail.com>
2025-06-05 01:05:07 +0300

    Исправления для Jacoco SonarCloud

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("org.sonarqube") version "6.2.0.5505"
+    id("jacoco")
 }
 
 group = "hexlet.code"
@@ -11,12 +12,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jacoco:org.jacoco.core:0.8.11")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 sonar {
@@ -29,4 +32,18 @@ sonar {
 
 tasks.withType<JavaCompile>() {
     options.compilerArgs.add("-Xlint:unchecked")
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+    reportsDirectory.set(layout.buildDirectory.dir("reports/jacoco"))
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
 }
